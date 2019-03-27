f291fb63c8008058a844b789baeb21c60496261a
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-27 01:07:03 +0000

    set up db credentials, some routing

--- database/index.js
+++ database/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const knex = require('knex')({
   client: 'pg',
   connection: {
     host: 'localhost',
-    database: 'clock_challenge'
+    database: 'clock_challenge',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
   }
 });
 
--- routes/api/clock_challenge.js
+++ routes/api/clock_challenge.js
@@ -4,9 +4,18 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../database')
 
-router.get('/', function(req,res) {
-  res.send('Hello')
-  db.select().from('')
+router.get('/', function(req, res) {
+  db.select().from('teachers').then(function(data) {
+    res.send(data)
+  })
+})
+
+router.post('/sessions', function(req, res) {
+  console.log(req.body)
+  // db.insert().returning('*').into('').then(function(data) {
+  //   res.send(data)
+  // })
+  res.send('hello')
 })
 
 module.exports = router
