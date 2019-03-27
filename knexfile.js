e266fc5c2ec02b251cdc3780c493fefa37b62ad1
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-27 03:53:28 +0000

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,52 @@
+// Update with your config settings.
+
 require('dotenv').config();
 
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    database: 'clock_challenge',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
-  }
-});
+module.exports = {
 
-module.exports = knex;
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: 'localhost',
+      database: 'clock_challenge',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'clock_challenge',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: process.env.DATABASE_URL + '?ssl=true',
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'migrations'
+  //   }
+  // }
+
+};
