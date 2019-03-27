6256343a6303fe5741055b6dba5b37c2fb1e1238
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-27 12:15:00 +0000

    fixed seed file

--- database/migrations/20190327055500_create_schema.js
+++ database/migrations/20190327055500_create_schema.js
@@ -19,7 +19,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('teachers'),
-    knex.schema.dropTable('sessions')
+    knex.schema.dropTable('sessions'),
+    knex.schema.dropTable('teachers')
   ]);
 }
